f4efe536a667778aa6ba87e996ceefbceb842724
Alyona-GS <asgor1994@gmail.com>
2022-06-23 00:57:44 +0400

    init: even_numbers.js

---
+++ even_numbers.js
@@ -0,0 +1,29 @@
+var i = 1;
+do {
+
+  if (!(i % 2)) {
+    console.log(i);
+  }
+
+  i++;
+
+} while(i <= 20);
+
+
+
+var i = 1;
+while(i <= 20) {
+
+  if (!(i % 2)) {
+    console.log(i);
+  }
+
+  i++;
+}
+
+
+for(let i = 1; i <= 20; i++) {
+  if (!(i % 2)) {
+    console.log(i);
+  }
+}
