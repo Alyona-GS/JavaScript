ceacb4cf938e241187df873f5cc2176bad060209
Alyona-GS <asgor1994@gmail.com>
2022-06-25 16:38:02 +0400

    init: text_to_words.js

---
+++ lesson_2/text_to_words.js
@@ -0,0 +1,19 @@
+function isCapitalized(word) {
+  return /[A-Z]/.test(word.charAt(0))
+}
+
+const wordObject = function (word) {
+  return word = {
+    word: word,
+    length: word.length,
+    isCapitalized: isCapitalized(word)
+  }
+};
+
+function textToWords (text) {
+  let words = text.replace(/[.,?!()]/g, " ")
+                  .trim()
+                  .split(/[\s]+/);
+
+  return words.map(wordObject)
+};
